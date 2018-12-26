32ceb9104233aa4463aeb57949c266a6d806cb3a
Prabesh Bhaskaran <prabeshb@gmail.com>
2018-12-26 18:39:31 -0500

    Alerts, Toasts & Loading

--- conFusion/src/pages/dishdetail/dishdetail.ts
+++ conFusion/src/pages/dishdetail/dishdetail.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import { Dish } from '../../shared/dish';
 import { Comment } from '../../shared/comment';
 import { FavoriteProvider } from '../../providers/favorite/favorite';
@@ -26,6 +26,7 @@ export class DishdetailPage {
 
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
+    private toastCtrl: ToastController,
     @Inject('BaseURL') private BaseURL,
     private favoriteservice: FavoriteProvider ) {
       this.dish = navParams.get('dish');
@@ -46,5 +47,10 @@ export class DishdetailPage {
   addToFavorites() {
     console.log('Adding to favorites', this.dish.id);
     this.favorite = this.favoriteservice.addFavorite(this.dish.id);
+    this.toastCtrl.create({
+      message: 'Dish '+ this.dish.id + " added as a favorite successfully",
+      position: 'middle',
+      duration: 3000
+    }).present();
   }
 }
--- conFusion/src/pages/favorites/favorites.ts
+++ conFusion/src/pages/favorites/favorites.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { IonicPage, NavController, NavParams, ItemSliding } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ItemSliding, ToastController, LoadingController, AlertController } from 'ionic-angular';
 import { FavoriteProvider  } from '../../providers/favorite/favorite';
 import { Dish } from '../../shared/dish';
 
@@ -22,6 +22,9 @@ export class FavoritesPage implements OnInit {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     private favoriteservice: FavoriteProvider,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private alertCtrl: AlertController,
     @Inject('BaseURL') private BaseURL) {
   }
 
@@ -37,9 +40,39 @@ export class FavoritesPage implements OnInit {
 
   deleteFavorite(item: ItemSliding, id: number) {
     console.log('delete', id);
-    this.favoriteservice.deleteFavorite(id)
-    .subscribe(favorites => this.favorites = favorites,
-      errmess => this.errMess = errmess);
+
+    let alert = this.alertCtrl.create({
+      title: 'Confirm Title',
+      message: 'Do you want to delete Favorite '+ id,
+      buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Delete cancelled');
+        }
+      },
+      {
+        text: 'Delete',
+        handler: () => {
+          let loading = this.loadingCtrl.create({
+            content: 'Deleting . . .'
+          });
+          let toast = this.toastCtrl.create({
+            message: 'Dish '+ id + ' deleted Successfully',
+            duration: 3000
+          });
+          loading.present();
+
+          this.favoriteservice.deleteFavorite(id)
+          .subscribe(favorites => { this.favorites = favorites; loading.dismiss(); toast.present(); },
+            errmess => { this.errMess = errmess; loading.dismiss(); });
+        }
+      }
+    ]
+    });
+
+    alert.present();
 
     item.close();
   }
